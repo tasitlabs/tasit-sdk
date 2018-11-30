36d285c4a025c083aa9a03f296dedb9ff5855232
Marcelo Morgado <cmarcelom@gmail.com>
2018-11-30 20:46:29 +0000

    test correction

--- packages/tasit-sdk/src/index.test.js
+++ packages/tasit-sdk/src/index.test.js
@@ -2,7 +2,7 @@ import { Account } from "./index.js";
 import { expect, assert } from "chai";
 
 describe("Account", function() {
-  it("should create an random wallet", async function() {
+  it("should create a random wallet", async function() {
     let w = await Account.create();
     expect(w.address).to.have.lengthOf(42);
   });
