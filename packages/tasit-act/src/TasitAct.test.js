e081445017495e029d383f0022edf59ed635ee44
Marcelo Morgado <cmarcelom@gmail.com>
2018-12-20 17:47:06 +0000

    https://github.com/tasitlabs/TasitSDK/pull/71#discussion_r243327505

--- packages/tasit-act/src/TasitAct.test.js
+++ packages/tasit-act/src/TasitAct.test.js
@@ -4,6 +4,7 @@ import { expect, assert } from "chai";
 // Note: SimpleStorage.json is originally genarated by `tasit-contracts` and was pasted here manually
 // See https://github.com/tasitlabs/TasitSDK/issues/45
 import { abi as contractABI } from "./helpers/SimpleStorage.json";
+
 // Note: Under the current `tasit-contracts` setup SimpleStorage aways will deployed with this address
 // See https://github.com/tasitlabs/TasitSDK/pull/59#discussion_r242258739
 const contractAddress = "0x6C4A015797DDDd87866451914eCe1e8b19261931";
