37b781afd7f89faaa48ed37705a1b616ef314705
Marcelo Morgado <cmarcelom@gmail.com>
2019-01-18 16:56:05 +0000

    nft contract

---
+++ packages/tasit-contracts/contracts/SimpleNFT.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
+
+contract SimpleNFT is ERC721Full, ERC721Mintable {
+  constructor() public ERC721Full("Simple NFT", "SNFT") {
+
+  }
+}
--- packages/tasit-contracts/migrations/2_deploy_contracts.js
+++ packages/tasit-contracts/migrations/2_deploy_contracts.js
@@ -2,6 +2,9 @@ var SimpleStorageWithRemoved = artifacts.require(
   "./SimpleStorageWithRemoved.sol"
 );
 
+var SimpleNFT = artifacts.require("./SimpleNFT.sol");
+
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorageWithRemoved, "Hello World!");
+  deployer.deploy(SimpleNFT);
 };
