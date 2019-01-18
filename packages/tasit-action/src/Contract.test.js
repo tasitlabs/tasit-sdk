6b2e98fc278729ca920148d31df694d7b2a7885f
Marcelo Morgado <cmarcelom@gmail.com>
2019-01-18 17:47:00 +0000

    tests update

--- packages/tasit-action/src/Contract.test.js
+++ packages/tasit-action/src/Contract.test.js
@@ -1,16 +1,12 @@
 import Contract from "./Contract";
 import Account from "tasit-account";
 
-// Note:  Using dist file because babel doesn't compile node_modules files.
-// Any changes on src should be followed by compilation to avoid unexpected behaviors.
-import { createFromPrivateKey } from "tasit-account/dist/testHelpers/helpers";
-
 // Note: SimpleStorage.json is originally genarated by `tasit-contracts` and was pasted here manually
 // See https://github.com/tasitlabs/TasitSDK/issues/45
-import { abi as contractABI } from "./testHelpers/SimpleStorage.json";
+import { abi as contractABI } from "./testHelpers/SimpleStorageWithRemoved.json";
 
-// Note: Under the current `tasit-contracts` setup SimpleStorage aways will deployed with this address
-// See https://github.com/tasitlabs/TasitSDK/pull/59#discussion_r242258739
+// Note: Under the current `tasit-contracts` setup SimpleStorageWithRemoved aways will deployed with this address
+// See https://github.com/tasitlabs/TasitSDK/issues/138
 const contractAddress = "0x6C4A015797DDDd87866451914eCe1e8b19261931";
 
 describe("TasitAction.Contract", () => {
--- packages/tasit-action/src/testHelpers/mochaSetup.js
+++ packages/tasit-action/src/testHelpers/mochaSetup.js
@@ -20,3 +20,10 @@ global.createSnapshot = createSnapshot;
 global.revertFromSnapshot = revertFromSnapshot;
 global.wait = wait;
 global.waitForEthersEvent = waitForEthersEvent;
+
+// External helpers
+//
+// Note:  Using dist file because babel doesn't compile node_modules files.
+// Any changes on src should be followed by compilation to avoid unexpected behaviors.
+import { createFromPrivateKey } from "tasit-account/dist/testHelpers/helpers";
+global.createFromPrivateKey = createFromPrivateKey;
