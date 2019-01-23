80b3f14b49ef11dda38375d5c7277766ba709c29
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-01-23 17:33:24 +0000

    Update packages/tasit-contracts/decentraland/scripts/migrate.sh
    Co-Authored-By: marcelomorgado <cmarcelom@gmail.com>

--- packages/tasit-contracts/decentraland/scripts/migrate.sh
+++ packages/tasit-contracts/decentraland/scripts/migrate.sh
@@ -14,6 +14,8 @@ cd $DECENTRALAND_DIR/mana && npx truffle migrate
 
 # land
 cp $DECENTRALAND_DIR/scripts/3_land_registry.js $DECENTRALAND_DIR/land/migrations
+# land project has that network pointing to 8454 port of ganache
+# the default develop network is pointing to 18545 and isn't working
 cd $DECENTRALAND_DIR/land && npx truffle migrate --network livenet
 
 # marketplace-contracts
