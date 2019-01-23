3b74ed282e604a03304db87721eab75073a0c3c3
Marcelo Morgado <cmarcelom@gmail.com>
2019-01-23 14:17:39 +0000

    using npx on migrate script

--- packages/tasit-contracts/decentraland/scripts/migrate.sh
+++ packages/tasit-contracts/decentraland/scripts/migrate.sh
@@ -10,8 +10,8 @@ DECENTRALAND_DIR="$PROJECT_DIR/decentraland"
 
 # mana
 cp $DECENTRALAND_DIR/scripts/3_mana_token.js $DECENTRALAND_DIR/mana/migrations
-cd $DECENTRALAND_DIR/mana && ./node_modules/.bin/truffle migrate
+cd $DECENTRALAND_DIR/mana && npx truffle migrate
 
 # land
 cp $DECENTRALAND_DIR/scripts/3_land_registry.js $DECENTRALAND_DIR/land/migrations
-cd $DECENTRALAND_DIR/land && ./node_modules/.bin/truffle migrate --network livenet
+cd $DECENTRALAND_DIR/land && npx truffle migrate --network livenet
