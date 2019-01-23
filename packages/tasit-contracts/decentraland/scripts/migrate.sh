92b0a67f0871dff4882dec8d4e09d7cd756127d1
Marcelo Morgado <cmarcelom@gmail.com>
2019-01-23 13:37:16 +0000

    migrate script fix 2

--- packages/tasit-contracts/decentraland/scripts/migrate.sh
+++ packages/tasit-contracts/decentraland/scripts/migrate.sh
@@ -2,10 +2,14 @@
 PROJECT_DIR=$1
 DECENTRALAND_DIR="$PROJECT_DIR/decentraland"
 
+echo "##########"
+echo "decentraland dir = $DECENTRALAND_DIR"
+echo "##########"
+
 # mana
 cp $DECENTRALAND_DIR/scripts/3_mana_token.js $DECENTRALAND_DIR/mana/migrations
-cd $DECENTRALAND_DIR/mana && npx truffle migrate
+cd $DECENTRALAND_DIR/mana && ./node_modules/.bin/truffle migrate
 
 # land
 cp $DECENTRALAND_DIR/scripts/3_land_registry.js $DECENTRALAND_DIR/land/migrations
-cd $DECENTRALAND_DIR/land && npx truffle migrate --network livenet
+cd $DECENTRALAND_DIR/land && ./node_modules/.bin/truffle migrate --network livenet
