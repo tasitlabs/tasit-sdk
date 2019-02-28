f5ead9aaad468ef0a07785e30b0cdf8925182530
Marcelo Morgado <cmarcelom@gmail.com>
2019-02-28 21:19:26 +0000

    CI fix

--- packages/tasit-contracts/contracts/MyERC20Full.sol
+++ packages/tasit-contracts/contracts/MyERC20Full.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
 contract MyERC20Full is ERC20Detailed, ERC20Mintable, ERC20Burnable {
   constructor() public ERC20Detailed("ERC20Full", "ERC20", 18) {
