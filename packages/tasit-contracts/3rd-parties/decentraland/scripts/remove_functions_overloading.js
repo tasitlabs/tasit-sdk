05edb403049e7d60a8338a68aa9e47bc2bde6608
Marcelo Morgado <cmarcelom@gmail.com>
2019-03-07 14:06:13 +0000

    Improve overloaded funcions removal script

--- packages/tasit-contracts/3rd-parties/decentraland/scripts/remove_functions_overloading.js
+++ packages/tasit-contracts/3rd-parties/decentraland/scripts/remove_functions_overloading.js
@@ -4,40 +4,44 @@ const fs = require("fs");
 // Calling overloaded function was only allowed since Truffle v5 and land project uses truffle v4.1.13
 // See more: https://github.com/trufflesuite/truffle/issues/737#issuecomment-454892913
 // As workaround solution, unused functions should be removed from abi
-const removeFromEstateRegistry = () => {
-  const filePath = "../land/build/contracts/EstateRegistry.json";
-  const rawdata = fs.readFileSync(filePath);
-  const contractJson = JSON.parse(rawdata);
-  const abi = contractJson.abi;
+
+const removeFunctionsFromABI = (contractJsonPath, removalCondition) => {
+  const rawdata = fs.readFileSync(contractJsonPath);
+  let contractJson = JSON.parse(rawdata);
+  const { abi } = contractJson;
 
   const toRemove = json => {
     const isFunction = json.type === "function";
-
-    return isFunction && json.name === "initialize" && json.inputs.length != 3;
+    return isFunction && removalCondition(json);
   };
 
-  contractJson.abi = abi.filter(json => !toRemove(json));
+  const abiAfterRemovals = abi.filter(json => !toRemove(json));
+  contractJson = { ...contractJson, abi: abiAfterRemovals };
 
   const data = JSON.stringify(contractJson, null, 2);
-  fs.writeFileSync(filePath, data);
+  fs.writeFileSync(contractJsonPath, data);
+};
+
+const removeFromEstateRegistry = () => {
+  const filePath = "../land/build/contracts/EstateRegistry.json";
+
+  // Keep only the initialize function with 3 arguments
+  const removalCondition = f => {
+    return f.name === "initialize" && f.inputs.length != 3;
+  };
+
+  removeFunctionsFromABI(filePath, removalCondition);
 };
 
 const removeFromMarketplace = () => {
   const filePath = "../marketplace-contracts/build/contracts/Marketplace.json";
-  const rawdata = fs.readFileSync(filePath);
-  const contractJson = JSON.parse(rawdata);
-  const abi = contractJson.abi;
 
-  const toRemove = json => {
-    const isFunction = json.type === "function";
-
-    return isFunction && json.name === "initialize" && json.inputs.length != 3;
+  // Keep only the initialize function with 3 arguments
+  const removalCondition = f => {
+    return f.name === "initialize" && f.inputs.length != 3;
   };
 
-  contractJson.abi = abi.filter(json => !toRemove(json));
-
-  const data = JSON.stringify(contractJson, null, 2);
-  fs.writeFileSync(filePath, data);
+  removeFunctionsFromABI(filePath, removalCondition);
 };
 
 removeFromEstateRegistry();
