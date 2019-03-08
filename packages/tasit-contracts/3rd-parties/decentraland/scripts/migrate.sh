378ef1ab9bca614a2af0f57e30ce4cc2e05c5542
Marcelo Morgado <cmarcelom@gmail.com>
2019-03-08 17:08:26 +0000

    Refactoring migration files

--- packages/tasit-contracts/3rd-parties/decentraland/scripts/migrate.sh
+++ packages/tasit-contracts/3rd-parties/decentraland/scripts/migrate.sh
@@ -29,17 +29,17 @@ node $DECENTRALAND_DIR/scripts/remove_functions_overloading.js
 
 ### MIGRATE
 # mana
-cp $DECENTRALAND_DIR/scripts/3_mana_migrations.js $DECENTRALAND_DIR/mana/migrations
+cp $DECENTRALAND_DIR/scripts/mana/3_mana_migrations.js $DECENTRALAND_DIR/mana/migrations
 cd $DECENTRALAND_DIR/mana && npx truffle migrate
 
 # land
-cp $DECENTRALAND_DIR/scripts/3_land_migrations.js $DECENTRALAND_DIR/land/migrations
+cp $DECENTRALAND_DIR/scripts/land/3_land_migrations.js $DECENTRALAND_DIR/land/migrations
 # land project has that network pointing to 8454 port of ganache
 # the default develop network is pointing to 18545 and isn't working
 cd $DECENTRALAND_DIR/land && npx truffle migrate --network livenet
 
 # marketplace-contracts
-cp $DECENTRALAND_DIR/scripts/3_marketplace-contracts_migrations.js $DECENTRALAND_DIR/marketplace-contracts/migrations
+cp $DECENTRALAND_DIR/scripts/marketplace-contracts/3_marketplace-contracts_migrations.js $DECENTRALAND_DIR/marketplace-contracts/migrations
 cd $DECENTRALAND_DIR/marketplace-contracts && npx truffle migrate
 
 
