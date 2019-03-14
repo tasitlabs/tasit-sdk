d29249066bf40d6feab73bf35f53b3066c209cd1
Marcelo Morgado <cmarcelom@gmail.com>
2019-03-14 16:48:49 +0000

    NPM scripts adjustments

--- packages/tasit-contracts/3rd-parties/decentraland/scripts/migrate.sh
+++ packages/tasit-contracts/3rd-parties/decentraland/scripts/migrate.sh
@@ -11,7 +11,3 @@ DECENTRALAND_DIR="$PROJECT_DIR/3rd-parties/decentraland"
 NETWORK="development";
 MNEMONIC="beach swap combine paper music cook electric bullet trust actress liquid asthma";
 INFURA_TOKEN="974bd2e667b246f29d2589a59600530e";
-
-### POPULATE BLOCKCHAIN
-#cp $DECENTRALAND_DIR/scripts/config/$NETWORK.js $DECENTRALAND_DIR/scripts/config/default.js
-#cd $PROJECT_DIR && npx babel-node $DECENTRALAND_DIR/scripts/decentralandOrdersLoad.js
