ab286877bd3833a429babf72c864964ab19c95ad
Marcelo Morgado <cmarcelom@gmail.com>
2019-03-14 21:04:05 +0000

    review request

--- packages/tasit-contracts/migrations/4_gnosis_contracts.js
+++ packages/tasit-contracts/migrations/4_gnosis_contracts.js
@@ -2,14 +2,14 @@ var ProxyFactory = artifacts.require("./ProxyFactory.sol");
 var GnosisSafe = artifacts.require("./GnosisSafe.sol");
 
 module.exports = (deployer, network, accounts) => {
-  const johnAddress = accounts[9];
+  const signer = accounts[9];
 
   // Workaround to write async/await migration scripts
   // See more: https://github.com/trufflesuite/truffle/issues/501#issuecomment-332589663
   deployer.then(async () => {
     const proxyFactory = await deployer.deploy(ProxyFactory);
     const safe = await deployer.deploy(GnosisSafe);
-    const owners = [johnAddress];
+    const owners = [signer];
     const threshold = 1;
     // Contract address for optional delegate call
     const to = "0x0000000000000000000000000000000000000000";
