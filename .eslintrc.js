1bd71c514f1e3ab969ba5bc617880b7930416846
cmarcelo <cmarcelom@gmail.com>
2019-05-02 22:20:15 +0100

    Installing eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  plugins: ["prettier"],
+  extends: ["eslint:recommended", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
