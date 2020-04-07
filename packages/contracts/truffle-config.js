2e0d70622f7bfdaf88f6d65340c66d63565363d7
Paul Cowgill <pauldavidcowgill@gmail.com>
2020-04-07 11:42:41 -0500

    Truffle contract optimizer setting

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -116,10 +116,10 @@ module.exports = {
       // version: "0.5.2", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
       //  evmVersion: "byzantium"
       // }
     },
